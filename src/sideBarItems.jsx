62cf962544bcd2407b7d1f514545ee4d616fdbb3
Zipeng Yang <zipengcode@gmail.com>
2020-10-10 20:37:59 +0100

    init - Mac 10 Oct

---
+++ src/sideBarItems.jsx
@@ -0,0 +1,59 @@
+export default {
+    "data" : [
+      {
+          "name": "OverView",
+          "url": "/item1"
+      },
+      {
+        "name": "Item2",
+        "url": "/item2"
+      },
+      {
+        "name": "Sales Report Pack",
+        "children": [
+          {
+            "name": "Child31",
+            "url": "/child31"
+          },
+          {
+            "name": "Child32",
+            "url": "/child32"
+          },
+          {
+            "name": "Child33",
+            "url": "/child33"
+          }
+        ]
+      },
+      {
+        "name": "Management Accounts Pack",
+        "children": [
+          {
+            "name": "Child41",
+            "url": "/child41"
+          },
+          {
+            "name": "Child42",
+            "url": "/child42"
+          },
+          {
+            "name": "Child43",
+            "children": [
+              {
+                "name": "Child431",
+                "url": "/child431"
+              },
+              {
+                "name": "Child432",
+                "url": "/child432,"
+              },
+              {
+                "name": "Child433",
+                "url": "/child433"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
